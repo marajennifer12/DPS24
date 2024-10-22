2ab5ed1c4a514c2d39affcfb43986b4c5a98bd13
marajennifer12 <124807842+marajennifer12@users.noreply.github.com>
2024-10-22 09:30:55 -0600

    Update insert_user.php

--- CRUD_Testing_DPS/insert_user.php
+++ CRUD_Testing_DPS/insert_user.php
@@ -8,7 +8,7 @@ $marca = $_POST['marca'];
 $precio = $_POST['precio'];
 
 
-$sql = "INSERT INTO productos VALUES('$id_producto','$nombre','$marca','$precio')";
+$sql = "INSERT INTO productos VALUES('$id','$nombre','$marca','$precio')";
 $query = mysqli_query($con, $sql);
 
 if($query){
